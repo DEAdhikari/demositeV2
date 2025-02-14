30bd2f87a04b2ba67551134f38304732487637f1
DEAdhikari <adhikari.debraj9830@gmail.com>
2025-02-14 13:45:11 +0530

    Update indexP4.py

--- indexP4.py
+++ indexP4.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 
 # Load Excel file
 uploaded_file = st.file_uploader("Upload an Excel file", type=["xlsx"])
